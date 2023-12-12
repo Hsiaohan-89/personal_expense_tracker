b2a4f29857d55a22a8c37e5632fdb7c17b63a219
Hsiaohan-89 <>
2023-12-12 11:34:39 +0000

    Create a expense.py file and classes to store the information

---
+++ expense.py
@@ -0,0 +1,6 @@
+# The classes for the expenses to store our information
+class Expense:
+    def __init__(self, name, amount, category):
+        self.name = name
+        self.amount = amount
+        self.category = category
